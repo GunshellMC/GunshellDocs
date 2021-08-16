cd79889169835832d836adca9f1019e71b184aee
Jazzkuh <32953684+Jazzkuh@users.noreply.github.com>
2021-08-16 19:05:59 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,37 +1,50 @@
 // .vuepress/config.js
 module.exports = {
-	base: '/docs',
-	title: 'MT-Wapens',
-	description: 'Met deze plugin voeg je wapens toe aan jouw Minetopia server.',
-	head: [
-		['link', { rel: 'icon', href: '/mtwapens.png' }]
-	],
-	theme: "yuu",
-	themeConfig: {
-		logo: '/mtwapens.png',
-		nav: [
-			{ text: 'Discord', link: 'https://discord.gg/AvRpCUZ' },
-			{ text: 'Admin Panel', link: 'https://dash.mtwapens.nl' }
-		],
-		sidebar: [
-			{
-				title: 'MT-Wapens',
-				collapsable: false,
-				children: [
-					'/',
-					'/commands.md',
-					'/guide/',
-					'/config/',
-				]
-			},
-			{
-				title: 'Developers',
-				collapsable: true,
-				children: [
-					'/api.md',
-					'/naming-conventions.md',
-				]
-			}
-		]
-	}
+    base: '/',
+    postcss: {
+        plugins: [
+            require('css-prefers-color-scheme/postcss'),
+            require('autoprefixer')
+        ]
+    },
+    title: 'MT-Wapens',
+    description: 'Met deze plugin voeg je wapens toe aan jouw Minetopia server.',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/mtwapens.png'
+        }]
+    ],
+    theme: "yuu",
+    themeConfig: {
+        logo: '/mtwapens.png',
+        nav: [{
+                text: 'Discord',
+                link: 'https://discord.gg/AvRpCUZ'
+            },
+            {
+                text: 'Admin Panel',
+                link: 'https://dash.mtwapens.nl'
+            }
+        ],
+        sidebar: [{
+                title: 'MT-Wapens',
+                collapsable: false,
+                children: [
+                    '/',
+                    '/commands.md',
+                    '/guide/',
+                    '/config/',
+                ]
+            },
+            {
+                title: 'Developers',
+                collapsable: true,
+                children: [
+                    '/api.md',
+                    '/naming-conventions.md',
+                ]
+            }
+        ]
+    }
 }
